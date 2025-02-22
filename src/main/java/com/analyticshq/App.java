512b1b4d5f7849cb60031eaed2d7e248bdaede72
ccravens <chad.cravens@analyticshq.com>
2025-02-22 04:10:43 +0000

    init

---
+++ src/main/java/com/analyticshq/App.java
@@ -0,0 +1,13 @@
+package com.analyticshq;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
